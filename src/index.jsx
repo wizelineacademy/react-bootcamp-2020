044eb3c83cbd6e0b6f465097a15d6f12868539a1
Luis Hiram Díaz <luishiram.ds@gmail.com>
2020-11-21 17:11:51 -0600

    starting app

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './components/App';
 
-import App from './components/App';
-import './global.css';
+// import './global.css';
 
 ReactDOM.render(
   <React.StrictMode>
