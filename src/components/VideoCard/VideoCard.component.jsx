53d0ced3eb45d56cbb5dd0a72e0bf81e3ff8c354
adantop <>
2020-11-27 23:00:45 +0000

    added videos

---
+++ src/components/VideoCard/VideoCard.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function VideoCard({title, url, thumbnail}) {
+  return <div>
+    <img src={thumbnail} alt={title}/>
+    <br/>
+    <a href={url} target="_blank" rel="noopener noreferrer">{title}</a>
+  </div>
+}
