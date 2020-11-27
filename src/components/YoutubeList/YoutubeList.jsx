08747aaf167f04957fce110257ed0b1ccc5d3a9c
EH <horta.enrique@gmail.com>
2020-11-27 09:51:41 -0600

    login

--- src/components/YoutubeList/YoutubeList.jsx
+++ src/components/YoutubeList/YoutubeList.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import useFetch from '../../hooks/useFetch';
-import demoData from '../../demodata.json';
+import demoData from '../../utils/response.json';
 import YoutubeListItem from './YoutubeListItem';
 import LoginContext from '../../state/LoginContext';
 
