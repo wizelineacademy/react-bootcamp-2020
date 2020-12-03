fcccbece47703a2384babc61d29d1667fcbdcd61
Benoit Borrel <benoit@borrel.com>
2020-12-03 01:56:23 -0500

    YouTube: refactor the API_* constants to be at the root of the component

--- src/utils/hooks/useYouTubeDataAPI.js
+++ src/utils/hooks/useYouTubeDataAPI.js
@@ -1,16 +1,16 @@
 import { useState, useEffect, useMemo } from 'react';
 
+const API_KEY = 'AIzaSyAcgmsZB2M27kvgZju-qtHhKiziG5dzYf8'; // benoit.borrel@gmail.com
+// const API_KEY = 'AIzaSyBfCTxfs9EgNkuHKRjful5zvSVVNymV34g'; // benoit@borrel.com
+const API_URL = 'https://www.googleapis.com/youtube/v3/';
+const API_PARAM_PART = 'snippet';
+const API_PARAM_TYPE = 'video';
+const API_PARAM_MAX_RESULTS = 10;
+
 export default function useYouTubeDataAPI(resource, method, parameters) {
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(null);
   const [videos, setVideos] = useState([]);
-
-  // const API_KEY = 'AIzaSyAcgmsZB2M27kvgZju-qtHhKiziG5dzYf8'; // benoit.borrel@gmail.com
-  const API_KEY = 'AIzaSyBfCTxfs9EgNkuHKRjful5zvSVVNymV34g'; // benoit@borrel.com
-  const API_URL = 'https://www.googleapis.com/youtube/v3/';
-  const API_PARAM_PART = 'snippet';
-  const API_PARAM_TYPE = 'video';
-  const API_PARAM_MAX_RESULTS = 2;
 
   function getFetchUrl(aResource, aMethod, aParameters) {
     const url = new URL(API_URL + aResource);
