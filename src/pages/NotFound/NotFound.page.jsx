ff5f65abc01c34cd68d450426eea1af07d53510b
adantop <>
2020-12-04 19:54:09 +0000

    add not found page

--- src/pages/NotFound/NotFound.page.jsx
+++ src/pages/NotFound/NotFound.page.jsx
@@ -8,9 +8,11 @@ export default function NotFoundPage() {
       <AppBar />
       <Grid container className="content">
         <Grid item xs={12} sm={8} md={10}>
-          <Typography variant="h1" component="h2">Woops! Not Found</Typography>
+          <Typography variant="h1" component="h2">
+            Woops! Not Found
+          </Typography>
         </Grid>
       </Grid>
     </>
-  )
+  );
 }
