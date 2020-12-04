772b661a98a03220673a356874cff1ae460ec921
chamedm <is708177@iteso.mx>
2020-12-03 18:42:24 -0600

    Deleting duplicate component

--- src/components/Navbar/Navbar.styles.jsx
+++ src/components/Navbar/Navbar.styles.jsx
@@ -10,19 +10,4 @@ const NavbarWrapper = styled.div`
   flex-direction: row;
 `;
 
-const PrimaryButton = styled.button`
-  background: #2a9d8f;
-  border: 1px solid #2a9d8f;
-  font-size: 1rem;
-  color: white;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-left: 1rem;
-  margin-right: 1rem;
-  line-height: 2rem;
-  &:hover {
-    background: #368f84;
-  }
-`;
-
-export { NavbarWrapper, PrimaryButton };
+export { NavbarWrapper };
