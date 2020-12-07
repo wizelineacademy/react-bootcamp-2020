b761216ad13e8684a363c100d56c3a16895a42e6
Fernando Espinosa <fernando.espinosa@payclip.com>
2020-12-07 00:20:40 -0600

    adding auth service

--- src/components/AppBar/TopAppBar.component.jsx
+++ src/components/AppBar/TopAppBar.component.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
@@ -6,6 +6,14 @@ import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import { useQuery } from 'react-query';
+import auth from '../../services/auth';
 import SearchVideos from '../SearchVideos';
 import { AuthContext } from '../../contexts/authContext/authContext';
 import logo from '../../assets/images/wla-small.png';
@@ -33,10 +41,99 @@ const useStyles = makeStyles((theme) => {
   };
 });
 
+function UserMenu() {
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+  return (
+    <div>
+      <IconButton
+        ref={anchorRef}
+        aria-controls={open ? 'menu-list-grow' : undefined}
+        aria-haspopup="true"
+        onClick={handleToggle}
+      >
+        <AccountCircleIcon />
+      </IconButton>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList
+                  autoFocusItem={open}
+                  id="menu-list-grow"
+                  onKeyDown={handleListKeyDown}
+                >
+                  <MenuItem onClick={handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={handleClose}>My account</MenuItem>
+                  <MenuItem onClick={handleClose}>Logout</MenuItem>
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </div>
+  );
+}
+
+function storeUser(res, action) {
+  if (res.data.length !== 1) return null;
+  action(res.data[0]);
+}
+
 export default function TopAppBar() {
   const classes = useStyles();
   const { authState, authActions } = useContext(AuthContext);
-
+  const { refetch } = useQuery(
+    ['/login', { user: 'wizeline', password: 'Rocks!' }],
+    auth.signIn,
+    {
+      refetchOnWindowFocus: false,
+      enabled: false,
+      onSuccess: (data) => storeUser(data, authActions.authStateChanged),
+    }
+  );
   return (
     <>
       <AppBar position="fixed" classes={{ root: classes.appBarRoot }}>
@@ -52,16 +149,10 @@ export default function TopAppBar() {
             </IconButton>
             <img src={logo} className={classes.logo} alt="Wizeline Academy" />
           </div>
-          <SearchVideos
-            onSubmit={() => authActions.authStateChanged({ name: 'fernando' })}
-          />
-          {authState.user && (
-            <Button size="medium" variant="outlined" color="secondary">
-              <AccountCircleIcon /> Sign In
-            </Button>
-          )}
+          <SearchVideos onSubmit={() => {}} />
+          {authState.user && <UserMenu />}
           {!authState.user && (
-            <Button size="medium" variant="outlined" color="secondary">
+            <Button onClick={refetch} size="medium" variant="outlined" color="secondary">
               <AccountCircleIcon /> Sign In
             </Button>
           )}
