cc3e1ec803994fd77addd83b8721b6420b92b32f
Malinali Becerril <mali@devf.mx>
2020-12-06 22:29:50 -0600

    add pages modifications

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -4,51 +4,38 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import AuthProvider from '../../providers/Auth';
 import HomePage from '../../pages/Home';
 import LoginPage from '../../pages/Login';
+import Favorites from '../../pages/Favorites';
 import NotFound from '../../pages/NotFound';
-import SecretPage from '../../pages/Secret';
+import Sidebar from '../Sidebar';
 import Private from '../Private';
-import Fortune from '../Fortune';
+// import Videos from '../Videos';
 import Layout from '../Layout';
-import { random } from '../../utils/fns';
+import Navbar from '../Navbar';
 
 function App() {
-  useLayoutEffect(() => {
-    const { body } = document;
-
-    function rotateBackground() {
-      const xPercent = random(100);
-      const yPercent = random(100);
-      body.style.setProperty('--bg-position', `${xPercent}% ${yPercent}%`);
-    }
-
-    const intervalId = setInterval(rotateBackground, 3000);
-    body.addEventListener('click', rotateBackground);
-
-    return () => {
-      clearInterval(intervalId);
-      body.removeEventListener('click', rotateBackground);
-    };
-  }, []);
+  useLayoutEffect(() => {}, []);
 
   return (
     <BrowserRouter>
       <AuthProvider>
         <Layout>
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route exact path="/login">
-              <LoginPage />
-            </Route>
-            <Private exact path="/secret">
-              <SecretPage />
-            </Private>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
-          <Fortune />
+          <Navbar />
+          <Sidebar>
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/login">
+                <LoginPage />
+              </Route>
+              <Private exact path="/favorites">
+                <Favorites />
+              </Private>
+              <Route path="*">
+                <NotFound />
+              </Route>
+            </Switch>
+          </Sidebar>
         </Layout>
       </AuthProvider>
     </BrowserRouter>
