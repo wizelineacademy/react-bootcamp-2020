ba15c4adeb8e50322a0e973ae4588bd901def151
Benoit Borrel <benoit@borrel.com>
2020-12-06 22:07:02 -0500

    authentication: refactor login form into own component; change authentication to use loginAPI

--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import { useHistory } from 'react-router';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
-import { useAuth } from '../../providers/Auth';
+import LoginForm from '../../components/Login/LoginForm.component';
 
 import './Login.styles.css';
 
 function LoginPage() {
-  const { login } = useAuth();
-  const history = useHistory();
-
-  function authenticate(event) {
-    event.preventDefault();
-    login();
-    history.goBack();
-  }
-
   return (
     <section id="cover" className="loginpage min-vh-100">
       <div id="cover-caption">
@@ -29,17 +17,7 @@ function LoginPage() {
             <Col className="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
               <h1 className="display-4 py-2 text-truncate">Welcome back!</h1>
               <div className="px-2">
-                <form onSubmit={authenticate} className="justify-content-center">
-                  <Form.Group>
-                    <Form.Control type="text" required placeholder="Username" />
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Control type="password" required placeholder="Password" />
-                  </Form.Group>
-                  <Button type="submit" className="btn-primary btn-lg">
-                    login
-                  </Button>
-                </form>
+                <LoginForm />
               </div>
             </Col>
           </Row>
--- src/providers/Auth/Auth.provider.jsx
+++ src/providers/Auth/Auth.provider.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext, useCallback } from 'react';
 
 import { AUTH_STORAGE_KEY } from '../../utils/constants';
 import { storage } from '../../utils/storage';
+import loginApi from '../../utils/loginApi';
 
 const AuthContext = React.createContext(null);
 
@@ -12,9 +13,12 @@ function useAuth() {
   }
   return context;
 }
+export { useAuth };
 
-function AuthProvider({ children }) {
+export default function AuthProvider({ children }) {
   const [authenticated, setAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const lastAuthState = storage.get(AUTH_STORAGE_KEY);
@@ -23,22 +27,28 @@ function AuthProvider({ children }) {
     setAuthenticated(isAuthenticated);
   }, []);
 
-  const login = useCallback(() => {
-    setAuthenticated(true);
-    storage.set(AUTH_STORAGE_KEY, true);
+  const login = useCallback((username, password) => {
+    loginApi(username, password).then(
+      (result) => {
+        setUser(result);
+        setAuthenticated(true);
+        storage.set(AUTH_STORAGE_KEY, true);
+      },
+      (err) => {
+        setError(err);
+      }
+    );
   }, []);
 
   const logout = useCallback(() => {
+    setUser(null);
     setAuthenticated(false);
     storage.set(AUTH_STORAGE_KEY, false);
   }, []);
 
   return (
-    <AuthContext.Provider value={{ login, logout, authenticated }}>
+    <AuthContext.Provider value={{ login, logout, authenticated, user, error }}>
       {children}
     </AuthContext.Provider>
   );
 }
-
-export { useAuth };
-export default AuthProvider;
