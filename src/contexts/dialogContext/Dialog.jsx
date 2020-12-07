7b755434e3642317df0be35ef4887249fa7852db
Fernando Espinosa <fernando.espinosa@payclip.com>
2020-12-07 01:50:33 -0600

    added Dialogs components and dialogs contexts

---
+++ src/contexts/dialogContext/Dialog.jsx
@@ -0,0 +1,36 @@
+import React, { useReducer, useEffect, useMemo } from 'react';
+import reducer, { initialState } from './reducer';
+import actions from './actions';
+import Dialog from '../../components/Dialogs/Login/Dialog.component';
+
+const DialogContext = React.createContext({});
+
+const DialogProvider = (props) => {
+  const [dialogState, dispatch] = useReducer(reducer, initialState);
+  const dialogActions = useMemo(() => actions(dispatch), []);
+  useEffect(() => {
+    dialogActions.registerDialog('miDialogo', false);
+    dialogActions.registerDialog('otro dialogo', false);
+  }, [dialogActions]);
+  return (
+    <DialogContext.Provider
+      value={{
+        dialogState,
+        dialogActions,
+      }}
+    >
+      <Dialog
+        title="este es mi dialogo"
+        open={
+          dialogState.dialogs.miDialogo ? dialogState.dialogs.miDialogo.isOpen : false
+        }
+        setOpen={(isOpen) => {
+          dialogActions.setOpen('miDialogo', isOpen);
+        }}
+      />
+      {props.children}
+    </DialogContext.Provider>
+  );
+};
+
+export { DialogProvider, DialogContext };
