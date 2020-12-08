b6a33540d4260f38b3e33386bab5dcf27a659d47
Erick Valdivia <alekzdev7@gmail.com>
2020-12-08 13:02:18 -0600

    Add guard

--- src/routing/ScreensRouter.jsx
+++ src/routing/ScreensRouter.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from '../screens/home/Home';
 import Detail from '../screens/detail/Detail';
+import { AuthContext } from '../store/contexts/AuthContext';
 
 const ScreensRouter = () => {
+  // eslint-disable-next-line
+  const [state, dispatch] = useContext(AuthContext);
+
   return (
     <div className="main">
       <Switch>
         <Route path="/detail/:id" component={Detail} />
-        <Route path="/fav" component={Home} />
+        {state.isAuth ? <Route path="/fav" component={Home} /> : null}
         <Route path="/" component={Home} />
       </Switch>
     </div>
