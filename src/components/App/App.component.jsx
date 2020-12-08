2c5e559d7d612cf6ffac11fa3a16a96574fd457a
Diego Guerrero Alexander <diegoguerreroalexander@gmail.com>
2020-12-08 09:49:56 -0600

    build test

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,13 +1,13 @@
 import React, { useLayoutEffect } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import AuthProvider from '../../providers/Auth';
-import HomePage from '../../pages/Home';
-import LoginPage from '../../pages/Login';
-import NotFound from '../../pages/NotFound';
-import SecretPage from '../../pages/Secret';
-import Private from '../Private';
-import Fortune from '../Fortune';
+// import HomePage from '../../pages/Home';
+// import LoginPage from '../../pages/Login';
+// import NotFound from '../../pages/NotFound';
+// import SecretPage from '../../pages/Secret';
+// import Private from '../Private';
+// import Fortune from '../Fortune';
 import Layout from '../Layout';
 import { random } from '../../utils/fns';
 import YouTube from '../YouTube.js'
