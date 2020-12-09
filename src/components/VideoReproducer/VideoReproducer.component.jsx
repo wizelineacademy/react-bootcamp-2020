981b0c96d274c8165b9f2b2492b2a6be759f1cd3
chamedm <is708177@iteso.mx>
2020-12-09 14:50:51 -0600

    Removing dev setup-deployment ready

--- src/components/VideoReproducer/VideoReproducer.component.jsx
+++ src/components/VideoReproducer/VideoReproducer.component.jsx
@@ -1,5 +1,4 @@
-// import React, { useEffect, useContext} from 'react';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useHistory } from 'react-router-dom';
 import { useAuth } from '../../providers/Auth';
@@ -14,12 +13,8 @@ import { SecondaryButton } from '../../styledComponents';
 
 import './VideoReproducer.style.css';
 
-// import { useYoutubeVideo } from '../../utils/hooks/useYoutubeVideo';
-// import { useChannel } from '../../utils/hooks/useChannel';
+import { useYoutubeVideo } from '../../utils/hooks/useYoutubeVideo';
 
-// --- DEV mocked setup --- ///
-import videoResultMocked from '../../utils/videoResultMocked.json';
-// --- End of DEV mocked setup --- ///
 
 function getVideoID(search) {
   const idRegex = new RegExp(/id\s*=\s*([\S\s]+)/);
@@ -31,45 +26,19 @@ function sliceDate(strDate) {
 }
 
 function VideoReproducer() {
-  // --- DEV mocked setup --- ///
-  // eslint-disable-next-line no-unused-vars
-  const [videoInformation, setVideoInformation] = React.useState(
-    videoResultMocked.items[0]
-  );
-  const isVideoRequestSuccessful = true;
-  const isVideoLoading = false;
-  // --- End of DEV mocked setup --- ///
-
-  // --- TODO: Call channel endpoint to retrieve name and avatar image
-  //   const [channelId, setChannelId] = React.useState('');
-  //   const [channelInformation, setChannelInformation] = React.useState({});
-  //   const { channelInfo, isChannelRequestSuccessful, isChannelLoading } = useChannel(
-  //     channelId
-  //   );
-  //   useEffect(() => {
-  //     if (!isVideoLoading) {
-  //       console.log(`setting id ${videoInformation.snippet.channelId}`);
-  //       setChannelId(videoInformation.snippet.channeld);
-  //     }
-  //   }, [isVideoLoading, videoInformation]);
-
-  //   useEffect(() => {
-  //     setChannelInformation(channelInfo);
-  //   }, [channelId]);
-  // --- End of TODO ---
   const { authenticated } = useAuth();
-  // const [videoInformation, setVideoInformation] = React.useState({});
+  const [videoInformation, setVideoInformation] = React.useState({});
   const history = useHistory();
   const videoIdParam = history.location.search;
   const videoId = getVideoID(videoIdParam);
 
-  // const { videoSelected, isVideoRequestSuccessful, isVideoLoading } = useYoutubeVideo(
-  //   videoId
-  // );
+  const { videoSelected, isVideoRequestSuccessful, isVideoLoading } = useYoutubeVideo(
+    videoId
+  );
 
-  // useEffect(() => {
-  //   setVideoInformation(videoSelected);
-  // }, [videoSelected]);
+  useEffect(() => {
+    setVideoInformation(videoSelected);
+  }, [videoSelected]);
 
   const renderReproducer = () => {
     if (isVideoRequestSuccessful)
