6c142873eff63aa07229b3eace955011cd0af42d
Luis <luisprogram23@gmail.com>
2020-12-08 18:26:06 -0600

    verifying a rout

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./components/app/App";
+import App from "./components/App/App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
