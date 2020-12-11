78b47102ec3ee185bf571b62b8ae39d8bd06996b
Benoit Borrel <benoit@borrel.com>
2020-12-10 23:53:43 -0500

    tests: add smoke test for App

---
+++ src/components/App/App.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.component';
+
+describe('Smoke tests', () => {
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+});
