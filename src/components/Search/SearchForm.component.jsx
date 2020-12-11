112942926db4cabb4f0e485b66856d30d6dddc1e
Benoit Borrel <benoit@borrel.com>
2020-12-10 22:54:47 -0500

    search: fix search form submitting which was not redirecting to search results

--- src/components/Search/SearchForm.component.jsx
+++ src/components/Search/SearchForm.component.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router';
 
 import SearchContext from '../../state/SearchContext';
 import useFormInput from '../../utils/hooks/useFormInput';
@@ -6,13 +7,15 @@ import useFormInput from '../../utils/hooks/useFormInput';
 export default function SearchForm() {
   const { searchTerm, setSearchTerm } = useContext(SearchContext);
   const searchInput = useFormInput(searchTerm, setSearchTerm);
+  const history = useHistory();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    history.push('/');
+  }
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
-    >
+    <form onSubmit={handleSubmit}>
       <input {...searchInput} placeholder="Search" className="form-control" />
     </form>
   );
