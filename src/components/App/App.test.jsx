72e9819b33a7652e64f0d23ed5b291545b20ab8a
Benoit Borrel <benoit@borrel.com>
2020-12-12 01:34:08 -0500

    app - test: add layout tests

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.component';
+import { render, screen } from '@testing-library/react';
 
-describe('Smoke tests', () => {
+import App from './App.component';
+import { SEARCH_PLACEHOLDER } from '../../utils/constants';
+
+describe('Smoke test', () => {
   it('renders', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+  });
+});
+
+describe('Layout tests', () => {
+  it('renders the home page, with header (navigation + search + login) + section', () => {
+    render(<App />);
+
+    expect(screen.getByRole('banner')).toBeInTheDocument();
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+    expect(screen.getByRole('banner')).toContainElement(screen.getByRole('navigation'));
+
+    expect(screen.getByPlaceholderText(SEARCH_PLACEHOLDER)).toBeInTheDocument();
+    expect(screen.getByRole('banner')).toHaveTextContent('login');
   });
 });
