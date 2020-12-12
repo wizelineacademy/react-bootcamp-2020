a0ccabd9a537668f52d301d54ed237aa7b954bf8
fergro <fergro96@hotmail.com>
2020-12-12 15:38:07 -0600

    Structure - .env file
    Adds .env file to protect the youtube api token

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
 export const AUTH_STORAGE_KEY = 'wa_cert_authenticated';
 export const BASE_URL = 'https://www.googleapis.com/youtube/v3/';
-export const API_TOKEN = 'AIzaSyCU9ohIwX7wOPR2AlUd9nQNMJ716Wb7om0';
-// export const API_TOKEN = 'AIzaSyAJDU0JXSvZylQslHO_cZDwwFmwQh9hWxo';
+export const API_TOKEN = process.env.REACT_APP_API_TOKEN;
