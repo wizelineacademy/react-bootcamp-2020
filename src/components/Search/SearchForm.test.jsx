d69eb24b1a4358b0f4b140f2333fd661b43a705f
Benoit Borrel <benoit@borrel.com>
2020-12-12 15:35:47 -0500

    search - test: simplify and complement SearchForm tests

--- src/components/Search/SearchForm.test.jsx
+++ src/components/Search/SearchForm.test.jsx
@@ -3,21 +3,19 @@ import { render, screen, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import App from '../App/App.component';
-import SearchContext from '../../state/SearchContext';
-import SearchForm from './SearchForm.component';
 import { SEARCH_TERM_DEFAULT } from '../../utils/constants';
 
 afterEach(cleanup);
 
 describe('Search form', () => {
+  it('renders a search box', () => {
+    render(<App />);
+
+    expect(screen.getByRole('searchbox')).toBeInTheDocument();
+  });
+
   it('updates search input value on typing', () => {
-    render(
-      <App>
-        <SearchContext>
-          <SearchForm />
-        </SearchContext>
-      </App>
-    );
+    render(<App />);
 
     userEvent.type(screen.getByRole('searchbox'), ' academy');
     expect(screen.getByRole('searchbox')).toHaveValue(`${SEARCH_TERM_DEFAULT} academy`);
