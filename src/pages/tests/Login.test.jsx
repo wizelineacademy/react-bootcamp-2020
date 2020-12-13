c63a1b3707e192dad9361c28a06a8b5ebd562446
Darthsoviet <31465914+Darthsoviet@users.noreply.github.com>
2020-12-13 13:54:26 -0600

    Revert "code formatted"
    This reverts commit 7258c6befcbe583b66c3ea247ae7d0cefc337f78.

--- src/pages/tests/Login.test.jsx
+++ src/pages/tests/Login.test.jsx
@@ -3,15 +3,15 @@ import { render, screen } from '@testing-library/react';
 import LoginPage from '../Login';
 import AuthProvider from '../../providers/Auth';
 
-describe('Login', () => {
-  it('renders title', () => {
-    const loginTitle = 'Welcome back!';
-    render(
-      <AuthProvider>
-        <LoginPage />
-      </AuthProvider>
-    );
-    const title = screen.getByRole('heading');
-    expect(title.innerHTML).toBe(loginTitle);
-  });
-});
+describe("Login",()=>{
+
+
+   it("renders title",()=>{
+      const loginTitle="Welcome back!";
+      render(<AuthProvider><LoginPage></LoginPage></AuthProvider>);
+      const title=screen.getByRole("heading");
+      expect(title.innerHTML).toBe(loginTitle);
+   })
+
+  
+})
