5330f9a6bdc537f935b78c1cb49121f494fbf17f
Laura Lujan <lauralujanhdez@gmail.com>
2020-12-13 18:05:48 -0600

    Add test to app comp

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -4,17 +4,7 @@ import AuthProvider from '../../providers/Auth/Auth.provider';
 import VideoProvider from '../../providers/Video/Video.provider';
 import UserPreferencesProvider from '../../providers/Preferences/UserPreferences.provider';
 import AppTheme from '../AppTheme/AppTheme';
-
-async function initGoogle() {
-  await new Promise((res, rej) => {
-    window.gapi.load('client', { callback: res, onerror: rej });
-  });
-
-  window.gapi.client.setApiKey(process.env.REACT_APP_YOUTUBE_KEY);
-
-  const x = await window.gapi.client.load('youtube', 'v3');
-  window.gapi.load('client:auth2', x);
-}
+import { initGoogle } from '../../api/gapi';
 
 function App() {
   const [loadDependencies, setLoadDependencies] = useState(false);
