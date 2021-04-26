7a50bf003b981bdd155d2ad43f22ee7f2f2b3b9d
Fava Narváez <fava@Favas-Mac-mini.local>
2021-04-26 13:50:36 -0500

    Final version challenge Faviola Narvaez Solved bugs

--- src/hooks/youtube.hook.js
+++ src/hooks/youtube.hook.js
@@ -1,32 +1,32 @@
-import { useState, useEffect, useCallback, useRef } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import youtube from '../api/youtube.api';
 
 import { useVideosContext } from '../context/context';
 
 const useVideoApi = (searchQuery) => {
-  const cache = useRef({});
+  // const cache = useRef({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const { state, setVideos } = useVideosContext();
 
   const onTermSubmit = useCallback(async () => {
     setLoading(true);
-    if (JSON.parse(window.localStorage.getItem('videosCache'))[searchQuery]) {
-      setVideos(JSON.parse(window.localStorage.getItem('videosCache'))[searchQuery]);
-    } else {
-      try {
-        const { data } = await youtube.get('/search', {
-          params: {
-            q: searchQuery,
-          },
-        });
-        cache.current[searchQuery] = data.items;
-        window.localStorage.setItem('videosCache', JSON.stringify(cache.current));
-        setVideos(data.items);
-      } catch (err) {
-        setError(err);
-      }
+    // if (JSON.parse(window.localStorage.getItem('videosCache'))[searchQuery]) {
+    // setVideos(JSON.parse(window.localStorage.getItem('videosCache'))[searchQuery]);
+    // } else {
+    try {
+      const { data } = await youtube.get('/search', {
+        params: {
+          q: searchQuery,
+        },
+      });
+      // cache.current[searchQuery] = data.items;
+      // window.localStorage.setItem('videosCache', JSON.stringify(cache.current));
+      setVideos(data.items);
+    } catch (err) {
+      setError(err);
     }
+    // }
     setLoading(false);
   }, [setVideos, searchQuery]);
 
