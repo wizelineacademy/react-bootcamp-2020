4ecd8fc0de847ab8be69e1147493b389ff442adb
Fava Narváez <fava@Favas-Mac-mini.local>
2021-04-28 16:37:50 -0500

    Final version challenge OAuth Faviola Narvaez build to netlify with correct api

--- src/hooks/youtube.hook.js
+++ src/hooks/youtube.hook.js
@@ -4,29 +4,22 @@ import youtube from '../api/youtube.api';
 import { useVideosContext } from '../context/context';
 
 const useVideoApi = (searchQuery) => {
-  // const cache = useRef({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const { state, setVideos } = useVideosContext();
 
   const onTermSubmit = useCallback(async () => {
     setLoading(true);
-    // if (JSON.parse(window.localStorage.getItem('videosCache'))[searchQuery]) {
-    // setVideos(JSON.parse(window.localStorage.getItem('videosCache'))[searchQuery]);
-    // } else {
     try {
       const { data } = await youtube.get('/search', {
         params: {
           q: searchQuery,
         },
       });
-      // cache.current[searchQuery] = data.items;
-      // window.localStorage.setItem('videosCache', JSON.stringify(cache.current));
       setVideos(data.items);
     } catch (err) {
       setError(err);
     }
-    // }
     setLoading(false);
   }, [setVideos, searchQuery]);
 
