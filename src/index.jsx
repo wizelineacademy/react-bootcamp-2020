6b965cc68fe601b988110e7ad2116eccde9f0111
Fava Narváez <fava@Favas-Mac-mini.local>
2021-04-28 16:31:35 -0500

    Final version challenge OAuth Faviola Narvaez build to netlify deleted my keys

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
+import dotenv from 'dotenv';
 
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 import './global.css';
 
+dotenv.config();
+const { REACT_APP_DOMAIN_OAUTH, REACT_APP_OAUTH_CLIENT_ID } = process.env;
+
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-h9ouf3h5.us.auth0.com"
-      clientId="mXPzc38j1P3FNar49PFgLRkAlA7v8z8N"
+      domain={REACT_APP_DOMAIN_OAUTH}
+      clientId={REACT_APP_OAUTH_CLIENT_ID}
       redirectUri={window.location.origin}
     >
       <Router>
